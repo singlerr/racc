43afb0b012a45b4eda09f51311a29d7c5df07dff
singlerr <singlerr@naver.com>
2024-08-11 23:24:27 +0900

    trying to define DFA using macro

--- automaton/src/dfa.rs
+++ automaton/src/dfa.rs
@@ -2,66 +2,109 @@ use std::collections::HashMap;
 use std::ops::Deref;
 use regex::Regex;
 
-/// A dfa must be qualified to have abilities: Start, End, Reject, Accept, Move on string input
-/// Each dfa node cell would be u32 data type, encoded to : 16-msb are dfa's option one, and 16-lsb are dfa's option two, each 8-msb are char, 8-lsb are index
-/// How to compile DFA to binary?
-struct DFA<T: StateStorage>{
+pub enum AutomataResponse{
+    ACCEPT,
+    REJECT
+}
+
+/// The first snapshot of DFA would be StateStorage based on HashMap
+/// TODO("Is there better state-symbol encoding?")
+struct DFA<T: StateStorage, F: AcceptanceChecker>{
     /// Current state id of this machine.
     /// Initial value must be 0 because in tradition, initial state id is 0.
-    state_index: u8,
-    /// In order to define 2nd array in runtime, boxing it.
-    /// Rows would be states and Columns symbols
-    state_table: T
+    state_index: usize,
+    /// More flexible state table
+    state_table: T,
+    /// More flexible machine state checker
+    acceptance_checker: F
 }
 
-
-pub trait StateStorage{
-    fn lookup(&self, from: u8, symbol: char) -> Option<u8>;
-    fn cache(&mut self, from: u8, to: u8, symbol: char);
+trait AcceptanceChecker{
+    fn accept(&self, state: usize) -> bool;
 }
 
-impl<T: StateStorage> DFA<T>{
+trait StateStorage{
+    fn lookup(&self, from: usize, symbol: char) -> Option<usize>;
+    fn cache(&mut self, from: usize, to: usize, symbol: char);
+}
+
+impl<T: StateStorage, F: AcceptanceChecker> DFA<T, F>{
     /// Encode destination of dfa with symbol input
-    fn encode(&mut self, s: char, from: u8, to: u8){
+    fn encode(&mut self, s: char, from: usize, to: usize){
         self.state_table.cache(from, to, s);
+
+        let table: [[char; 8];8] = {
+            let a = 0;
+            [['\0';8];8]
+        };
     }
 
     /// Consume input symbol to forward state machine
-    fn consume(&self, s: char) -> Option<u8> {
+    fn consume(&self, s: char) -> Option<usize> {
         self.state_table.lookup(self.state_index, s)
     }
 }
-fn a(){
 
+struct BaseStorage{
+    symbol_to_from: HashMap<char, usize>,
+    from_to_to: HashMap<usize, usize>
 }
 
-pub macro_rules! new_dfa {
-    ($state_size:expr, $symbol_size:expr) => {
+struct BaseAcceptanceChecker{
+    final_state: Vec<usize>
+}
 
-        struct GeneratedStorage_$state_size_$symbol_size{
-            state_table: [[char; $symbol_size]; $state_size]
-        };
+impl StateStorage for BaseStorage{
+    fn lookup(&self, from: usize, symbol: char) -> Option<usize> {
+        let from = self.symbol_to_from[symbol]?;
+        self.from_to_to[from]
+    }
 
-        impl StateStorage for GeneratedStorage_$state_size_$symbol_size{
-                fn lookup(&self, from: u8, symbol: char) -> Option<u8>{
-                    if(from >= &self.state_table.len() || from < 0)
-                        None()
-                    Some(&self.state_table[from][symbol])
-                }
-                fn cache(&mut self, from: u8, to: u8, symbol: char){
-                    &self.state_table[from][symbol] = to;
-                }
-        }
+    fn cache(&mut self, from: usize, to: usize, symbol: char) {
+        self.symbol_to_from.insert(symbol, from);
+        self.from_to_to.insert(from, to);
+    }
+}
 
-        DFA{
-            state_index: 0,
-            state_table: GeneratedStorage_$state_size_$symbol_size{
-                state_table =
+fn a() -> DFA<BaseStorage, BaseAcceptanceChecker>{
+}
+
+impl <T: StateStorage, F: AcceptanceChecker> DFA<T,F>{
+    pub fn transit(&mut self, symbol: char) -> Option<AutomataResponse>{
+        if let Some(next_state) = self.state_table.lookup(self.state_index, symbol){
+            return if self.acceptance_checker.accept(next_state) {
+                Some(AutomataResponse::ACCEPT)
+            } else {
+                self.state_index = next_state;
+                None
             }
+        } else {
+            Some(AutomataResponse::REJECT)
         }
-    };
+
+
+    }
+
 }
 
-fn main(){
-    let dfa = new_dfa!(16,16);
+#[macro_export]
+macro_rules! new_dfa {
+    ($state_size:expr, $symbol_size:expr, [$($y:expr),*]) => {
+                 /// Hacky things to make macro available to assign var in statement
+                 || -> DFA<BaseStorage,BaseAcceptanceChecker>{
+                    let base_acceptance_checker = BaseAcceptanceChecker{
+                        final_state: vec![$($y),*]
+                    };
+                    let base_storage = BaseStorage{
+                        symbol_to_from: HashMap::with_capacity($symbol_size),
+                        from_to_to: HashMap::with_capacity($state_size)
+                    };
+
+                    DFA{
+                        state_index: 0,
+                        state_table: base_storage,
+                        acceptance_checker: base_acceptance_checker
+                    }
+                }()
+    };
 }
--- automaton/src/lib.rs
+++ automaton/src/lib.rs
@@ -1,4 +1,5 @@
 mod dfa;
 fn main(){
-    let dfa = new_dfa!(16,16);
+    let dfa = new_dfa!(16,16, [1,2,3]);
+
 }
